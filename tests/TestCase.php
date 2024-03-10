ffcd6eea9a63c519696b86960763364f74368b96
Marick van Tuil <info@marickvantuil.nl>
2024-03-10 11:19:20 +0100

    Install Testbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,14 @@
 
 namespace Tests;
 
+use Illuminate\Testing\TestResponse;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Tests\Support\Kernel;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    use WithWorkbench;
+
     /**
      * Get package providers.  At a minimum this is the package being tested, but also
      * would include packages upon which our package depends, e.g. Cartalyst/Sentry
@@ -31,13 +35,36 @@ class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
+        if (! defined('ARTISAN_BINARY')) {
+            define('ARTISAN_BINARY', __DIR__ . '/../vendor/bin/testbench');
+        }
+
         foreach (glob(storage_path('framework/cache/data/*/*/*')) as $file) {
             unlink($file);
         }
+    }
 
-        $app->singleton(
-            \Illuminate\Contracts\Console\Kernel::class,
-            Kernel::class
-        );
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->resetLog();
+    }
+
+    public function assertLogged(string $message): void
+    {
+        $log = file_get_contents(storage_path('log.txt'));
+        $this->assertStringContainsString($message, $log);
+    }
+
+    public function assertLoggedLines(int $lines): void
+    {
+        $log = file_get_contents(storage_path('log.txt'));
+        $this->assertCount($lines, array_filter(explode("\n", $log)));
+    }
+
+    public function resetLog(): void
+    {
+        file_put_contents(storage_path('log.txt'), '');
     }
 }
